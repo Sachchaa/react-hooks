0e58b16b58347e0d228d068173c73d00e9036ab7
Sachin Kanishka <sachinkanishka001@gmail.com>
2021-05-10 00:40:27 +1000

    React-hooks updated -
    Covered UseContext hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { useReducer, useState } from 'react'
-
+import React from 'react'
+import FunctionContextComponent from './FunctionContextComponent'
+import { ThemeProvider } from './ThemeContext'
 
 function App() {
 
   return (
     <>
-
+      <ThemeProvider>
+        <FunctionContextComponent />
+      </ThemeProvider>
     </>
   );
 }
